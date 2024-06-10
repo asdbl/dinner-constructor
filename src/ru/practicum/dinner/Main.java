50654bf4fa9d66d168962b0d064bfea79607182f
Andrey Klyonin <andreyklyonin@gmail.com>
2024-06-10 11:06:11 +0300

    Added default case, if invalid command

--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -27,6 +27,8 @@ public class Main {
                     break;
                 case "3":
                     return;
+                default:
+                    System.out.println("Введена неверная комманда");
             }
         }
     }
